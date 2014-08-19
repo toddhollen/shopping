09389ce84cdac77e4d769337baf84ecdc0d9b2f8
toddhollen <toddhollen@gmail.com>
2014-08-19 17:38:19 -0600

    Beginning of Jquery

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	$('#store-add').on('click', function(){
-		alert('store-add clicked');
-		//Set  .store to "inline"\\
-		//Set .store to name inputted\\
-		//Hide Input and Button\\
+
+	//Click Add Store button\\
+	$('#store-input').on('click', 'button', function(){
+
+		//Hide Add Store Form\\
+		$(this).closest('#store-input').css('display','none');
+
+		//Add Store name entered to title\\
+		
+		
+		//Show Add Item Form\\
+		$('#list-item').slideDown(800);
+
+		//Show Shopping List
+		$('.shopping-list').slideDown(800);
+
+
 	});
+
+
+//Add items to shopping list\\
 	$('#item-add').on('click', function(){
 		alert('item-add clicked');
 		//add item to .list\\
