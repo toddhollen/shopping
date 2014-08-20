9b07c0ba65b43fb4768fc452f6038db76bd4924d
toddhollen <toddhollen@gmail.com>
2014-08-19 19:11:40 -0600

    Added Jquery

--- js/app.js
+++ js/app.js
@@ -6,25 +6,43 @@ $(document).ready(function() {
 		//Hide Add Store Form\\
 		$(this).closest('#store-input').css('display','none');
 
-		//Add Store name entered to title\\
-		
+		//Add store name to title\\
+		var storeName = $("#store-field").val();
+		$('.store').text(storeName)
 		
 		//Show Add Item Form\\
 		$('#list-item').slideDown(800);
 
 		//Show Shopping List
 		$('.shopping-list').slideDown(800);
-
-
 	});
 
+$('#store-input').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#store-add').click();
+		};
+	});	
 
 //Add items to shopping list\\
 	$('#item-add').on('click', function(){
-		alert('item-add clicked');
-		//add item to .list\\
+		
+		//Get to Pass
+		var item = $('<li></li>');
+		var itemText = $('#list-item input').val();
 
+		//Sets values to li
+		item.text(itemText);
+
+		//Adds it to list
+		$(".shopping-list ul.list").append(item);
 	});
 
+$('#list-item input').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#item-add').click();
+		};
+	});	
 
 });
